9ed1a58d72f21493d943284d470371d1a5cfb120
CzinkeM <mczinke-dev@proton.me>
2023-11-19 15:31:10 +0100

    Added permission request.

--- app/src/main/java/hu/mczinke/experimental_compose_stream/MainActivity.kt
+++ app/src/main/java/hu/mczinke/experimental_compose_stream/MainActivity.kt
@@ -22,6 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    RequestPermissionScreen(
+                        modifier = Modifier.fillMaxSize(),
+                    )
                 }
             }
         }
